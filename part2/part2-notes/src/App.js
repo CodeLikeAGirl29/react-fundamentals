dda087fee06335c3cc22696adcab2fd83c9b4e74
Lindsey Howard <miss_leading009@yahoo.com>
2021-10-30 18:16:30 -0700

    Getting data from the server

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -1,32 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
+import noteService from "./services/notes";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    };
+ useEffect(() => {
+   noteService.getAll().then((initialNotes) => {
+     setNotes(initialNotes);
+   });
+ }, []);
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  };
+const toggleImportanceOf = (id) => {
+  const note = notes.find((n) => n.id === id);
+  const changedNote = { ...note, important: !note.important };
+
+  noteService
+    .update(id, changedNote)
+    .then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+    })
+    .catch((error) => {
+      alert(`the note '${note.content}' was already deleted from server`);
+      setNotes(notes.filter((n) => n.id !== id));
+    });
+};
+
+ const addNote = (event) => {
+   event.preventDefault();
+   const noteObject = {
+     content: newNote,
+     date: new Date().toISOString(),
+     important: Math.random() > 0.5,
+   };
+
+   noteService.create(noteObject).then((returnedNote) => {
+     setNotes(notes.concat(returnedNote));
+     setNewNote("");
+   });
+ };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
